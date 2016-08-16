6b924678d198ce854dbff431ba23dac5cacae8a2
mefest <fral_@mail.ru>
2016-08-16 19:55:08 +0300

    include

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QDir>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +20,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QFile *file;
 };
 
 #endif // MAINWINDOW_H
